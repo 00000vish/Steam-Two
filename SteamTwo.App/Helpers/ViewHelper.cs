09c232821836e90ac09be389c6b2f4bc5810d2ee
vish <11762008+00000vish@users.noreply.github.com>
2023-03-08 21:03:35 -0800

    implement view helper

---
+++ SteamTwo.App/Helpers/ViewHelper.cs
@@ -0,0 +1,32 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+
+namespace SteamTwo.App.Helpers
+{
+    public static class ViewHelper
+    {
+        private static Dictionary<Type, MetroWindow> _viewDictionary = new();
+
+        public static void Register(MetroWindow window, params Type[] args)
+        {
+            foreach (var type in args)
+            {
+                if(_viewDictionary.ContainsKey(type))
+                    _viewDictionary.Remove(type);
+
+                _viewDictionary.Add(type, window);
+            }
+        }
+
+        public static MetroWindow GetView(object viewModel)
+        {
+            var type = viewModel.GetType();
+
+            if (!_viewDictionary.ContainsKey(type))
+                throw new Exception("View not registered");
+
+            return _viewDictionary[type];   
+        }
+    }
+}
