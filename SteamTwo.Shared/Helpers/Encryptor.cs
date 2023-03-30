027d4f39a077d5b1635dd0a84c13e60f4847c3d2
vish <11762008+00000vish@users.noreply.github.com>
2023-03-29 20:56:04 -0700

    Update encryptor.

--- SteamTwo.Shared/Helpers/Encryptor.cs
+++ SteamTwo.Shared/Helpers/Encryptor.cs
@@ -8,101 +8,120 @@ namespace SteamTwo.Shared.Helpers
 {
     public static class Encryptor
     {
-        private static string _masterKey = "6uh5GTFjUPvf7P";
-
-        private static readonly int _iterations = 2;
-        private static readonly int _keySize = 256;
-
-        private static readonly string _hash = "SHA1";
-        private static readonly string _salt = "qyotvsvbitb1fh70";
-        private static readonly string _vector = "wgcsg7si5sbspunf";
+        private static string _masterKey = "aPHsydtFsbC8jrxf";
+        private static string _iv = "Hh8B3RCBNYKyYr42";
 
         public static void SetMasterKey(string masterKey)
         {
-            _masterKey = masterKey; 
+            _masterKey = masterKey;
+
+            char[] charArray = _masterKey.ToCharArray();
+            Array.Reverse(charArray);
+            _iv =  new string(charArray);
         }
 
-        public static string Encrypt(string value)
+        public static string Encrypt(string data)
         {
-            return Encrypt(Aes.Create(), value, _masterKey);
-        }
+            var myAesKey = Encoding.UTF8.GetBytes(_masterKey);
+            var myAesIV = Encoding.UTF8.GetBytes(_iv);
 
-        public static string Decrypt(string value)
-        {
-            return Decrypt(Aes.Create(), value, _masterKey);
-        }
+            byte[] encrypted = EncryptStringToBytes_Aes(data, myAesKey, myAesIV);
 
-        private static string Encrypt<T>(T encryptAlg, string value, string password) where T : SymmetricAlgorithm
-        {
-            byte[] vectorBytes = Encoding.ASCII.GetBytes(_vector);
-            byte[] saltBytes = Encoding.ASCII.GetBytes(_salt);
-            byte[] valueBytes = Encoding.UTF8.GetBytes(value);
-
-            byte[] encrypted;
-            using (T cipher = encryptAlg)
-            {
-                PasswordDeriveBytes _passwordBytes =
-                    new PasswordDeriveBytes(password, saltBytes, _hash, _iterations);
-                byte[] keyBytes = _passwordBytes.GetBytes(_keySize / 8);
-
-                cipher.Mode = CipherMode.CBC;
-
-                using (ICryptoTransform encryptor = cipher.CreateEncryptor(keyBytes, vectorBytes))
-                {
-                    using (MemoryStream to = new MemoryStream())
-                    {
-                        using (CryptoStream writer = new CryptoStream(to, encryptor, CryptoStreamMode.Write))
-                        {
-                            writer.Write(valueBytes, 0, valueBytes.Length);
-                            writer.FlushFinalBlock();
-                            encrypted = to.ToArray();
-                        }
-                    }
-                }
-                cipher.Clear();
-            }
             return Convert.ToBase64String(encrypted);
         }
 
-        private static string Decrypt<T>(T encryptAlg, string value, string password) where T : SymmetricAlgorithm
+        public static string Decrypt(string data)
         {
-            byte[] vectorBytes = Encoding.ASCII.GetBytes(_vector);
-            byte[] saltBytes = Encoding.ASCII.GetBytes(_salt);
-            byte[] valueBytes = Convert.FromBase64String(value);
+            var myAesKey = Encoding.UTF8.GetBytes(_masterKey);
+            var myAesIV = Encoding.UTF8.GetBytes(_iv);
 
-            byte[] decrypted;
-            int decryptedByteCount = 0;
+            var encrypted = Convert.FromBase64String(data);
 
-            using (T cipher = encryptAlg)
+            return DecryptStringFromBytes_Aes(encrypted, myAesKey, myAesIV);
+        }
+
+        private static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
             {
-                PasswordDeriveBytes _passwordBytes = new PasswordDeriveBytes(password, saltBytes, _hash, _iterations);
-                byte[] keyBytes = _passwordBytes.GetBytes(_keySize / 8);
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
 
-                cipher.Mode = CipherMode.CBC;
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
 
-                try
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
                 {
-                    using (ICryptoTransform decryptor = cipher.CreateDecryptor(keyBytes, vectorBytes))
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
                     {
-                        using (MemoryStream from = new MemoryStream(valueBytes))
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
                         {
-                            using (CryptoStream reader = new CryptoStream(from, decryptor, CryptoStreamMode.Read))
-                            {
-                                decrypted = new byte[valueBytes.Length];
-                                decryptedByteCount = reader.Read(decrypted, 0, decrypted.Length);
-                            }
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+
+        private static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
                         }
                     }
                 }
-                catch (Exception)
-                {
-                    return string.Empty;
-                }
-
-                cipher.Clear();
             }
-            return Encoding.UTF8.GetString(decrypted, 0, decryptedByteCount);
+
+            return plaintext;
         }
-   
+
     }
 }
