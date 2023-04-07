94ef1c844135ab07f36af98c95d906a813995829
vish <11762008+00000vish@users.noreply.github.com>
2023-04-06 23:25:55 -0700

    Display added accounts.

--- SteamTwo.App/ViewModels/MainWindowViewModel.cs
+++ SteamTwo.App/ViewModels/MainWindowViewModel.cs
@@ -1,16 +1,18 @@
-﻿using MahApps.Metro.Controls.Dialogs;
+﻿using DynamicData;
+using DynamicData.Binding;
 using ReactiveUI;
-using SteamTwo.App.Helpers;
 using SteamTwo.App.Windows;
 using SteamTwo.Shared.Helpers;
+using System;
 using System.Diagnostics;
 using System.Reactive;
-using System.Security.AccessControl;
+using System.Reactive.Linq;
 
 namespace SteamTwo.App.ViewModels
 {
     class MainWindowViewModel : BaseViewModel
     {
+        public ObservableCollectionExtended<SteamAccountViewModel> SteamAccounts { get; }
         public ReactiveCommand<Unit, Unit> AddAccountCommand { get; }
         public ReactiveCommand<Unit, Unit> SettingsCommand { get; }
         public ReactiveCommand<Unit, Unit> GithubCommand { get; }
@@ -20,11 +22,20 @@ namespace SteamTwo.App.ViewModels
             AddAccountCommand = ReactiveCommand.Create(AddAccount);
             GithubCommand = ReactiveCommand.Create(VisitGithub);
             SettingsCommand = ReactiveCommand.Create(ShowSettings);
+
+
+            SteamAccounts = new();
+
+            AccountManager.Connect()
+                          .Transform(x => new SteamAccountViewModel(x))
+                          .ObserveOnDispatcher()
+                          .Bind(SteamAccounts)
+                          .DisposeMany()
+                          .Subscribe();
         }
 
         public void AddAccount()
         {
-            //forgive me mvvm
             AddAccountView addAcc = new();
             addAcc.Show();
         }
---
+++ SteamTwo.App/ViewModels/SteamAccountViewModel.cs
@@ -0,0 +1,36 @@
+﻿using ReactiveUI;
+using SteamTwo.Shared.Helpers;
+using SteamTwo.Shared.Models;
+using System.Reactive;
+
+namespace SteamTwo.App.ViewModels
+{
+    public class SteamAccountViewModel : BaseViewModel
+    {
+        public string Username { get; }
+        public ReactiveCommand<Unit, Unit> CopyUsernameCommand { get; }
+        public ReactiveCommand<Unit, Unit> CopyPasswordCommand { get; }
+
+        private SteamAccount _account;
+
+        public SteamAccountViewModel(SteamAccount account)
+        {
+            _account = account;
+
+            Username = _account.Username;
+
+            CopyUsernameCommand = ReactiveCommand.Create(CopyUsername);
+            CopyPasswordCommand = ReactiveCommand.Create(CopyPassword);
+        }
+
+        private void CopyUsername()
+        {
+            System.Windows.Clipboard.SetText(_account.Username);
+        }
+
+        private void CopyPassword()
+        {
+            System.Windows.Clipboard.SetText(_account.GetPassword());
+        }
+    }
+}
